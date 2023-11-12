2af4d212a8dc965ee811ecf78d9af08472457492
bigq <yum0e@protonmail.com>
2023-11-12 18:43:11 +0100

    feat: simple working REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use rustyline::{error::ReadlineError, DefaultEditor, Result};
+
+fn main() -> Result<()> {
+    let mut rl = DefaultEditor::new()?;
+
+    loop {
+        let readline = rl.readline("lildb > ");
+        match readline {
+            Ok(line) => {
+                if line == ".exit" {
+                    println!("Exiting...");
+                    break;
+                }
+                println!("Unrecognized command: {}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+
+    Ok(())
 }
