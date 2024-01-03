60f840e2bf751178e7f92a305285d186f5118d15
bigq <yum0e@protonmail.com>
2024-01-03 15:53:35 +0100

    feat: add logic to serialize the row

--- src/main.rs
+++ src/main.rs
@@ -32,14 +32,96 @@ impl std::fmt::Display for Statement {
     }
 }
 
+// this macro has been taken from this stack overflow post: https://stackoverflow.com/questions/61046063/how-to-get-the-size-of-a-struct-field-in-rust-without-instantiating-it
+macro_rules! field_size {
+    ($t:ident :: $field:ident) => {{
+        let m = core::mem::MaybeUninit::<$t>::uninit();
+        // According to https://doc.rust-lang.org/stable/std/ptr/macro.addr_of_mut.html#examples,
+        // you can dereference an uninitialized MaybeUninit pointer in addr_of!
+        // Raw pointer deref in const contexts is stabilized in 1.58:
+        // https://github.com/rust-lang/rust/pull/89551
+        let p = unsafe { core::ptr::addr_of!((*(&m as *const _ as *const $t)).$field) };
+
+        const fn size_of_raw<T>(_: *const T) -> usize {
+            core::mem::size_of::<T>()
+        }
+        size_of_raw(p)
+    }};
+}
+
+const COLUMN_USERNAME_SIZE: usize = 32;
+const COLUMN_EMAIL_SIZE: usize = 255;
+const ID_SIZE: usize = field_size!(Row::id);
+const USERNAME_SIZE: usize = field_size!(Row::username);
+const EMAIL_SIZE: usize = field_size!(Row::email);
+const ID_OFFSET: usize = 0;
+const USERNAME_OFFSET: usize = ID_OFFSET + ID_SIZE;
+const EMAIL_OFFSET: usize = USERNAME_OFFSET + USERNAME_SIZE;
+const ROW_SIZE: usize = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 #[derive(Debug)]
 pub struct Row {
-    pub id: u64,
-    pub username: String,
-    pub email: String,
+    pub id: u32,
+    username: [u8; COLUMN_USERNAME_SIZE],
+    email: [u8; COLUMN_EMAIL_SIZE],
+}
+
+impl Row {
+    pub fn serialize(&self, destination: [u8; ROW_SIZE]) -> [u8; ROW_SIZE] {
+        let mut serialized_row = destination;
+        serialized_row[ID_OFFSET..ID_OFFSET + ID_SIZE].copy_from_slice(&self.id.to_le_bytes());
+
+        serialized_row[USERNAME_OFFSET..USERNAME_OFFSET + USERNAME_SIZE]
+            .copy_from_slice(&self.username);
+
+        serialized_row[EMAIL_OFFSET..EMAIL_OFFSET + EMAIL_SIZE].copy_from_slice(&self.email);
+
+        serialized_row
+    }
+
+    pub fn username(&self) -> String {
+        let mut username = String::new();
+        for byte in &self.username {
+            if *byte == 0 {
+                break;
+            }
+            username.push(*byte as char);
+        }
+        username
+    }
+
+    pub fn email(&self) -> String {
+        let mut email = String::new();
+        for byte in &self.email {
+            if *byte == 0 {
+                break;
+            }
+            email.push(*byte as char);
+        }
+        email
+    }
+}
+
+impl std::fmt::Display for Row {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Row {{ id: {}, username: {}, email: {} }}",
+            self.id,
+            self.username(),
+            self.email()
+        )
+    }
 }
 
 fn main() -> Result<()> {
+    println!("size of val id:{}", ID_SIZE);
+    println!("offset of val id:{}", ID_OFFSET);
+    println!("size of val username:{}", USERNAME_SIZE);
+    println!("offset of val username:{}", USERNAME_OFFSET);
+    println!("size of val email:{}", EMAIL_SIZE);
+    println!("offset of val email:{}", EMAIL_OFFSET);
+    println!("size of val row:{}", ROW_SIZE);
+
     let mut rl = DefaultEditor::new()?;
 
     loop {
@@ -117,18 +199,41 @@ fn parsing_statement(line: &str) -> anyhow::Result<StatementCommandResult> {
             )));
         }
 
+        if input[2].len() > COLUMN_USERNAME_SIZE {
+            return Ok(StatementCommandResult::SyntaxError(format!(
+                "{}",
+                "Username is too long.".error()
+            )));
+        }
+
+        if input[3].len() > COLUMN_EMAIL_SIZE {
+            return Ok(StatementCommandResult::SyntaxError(format!(
+                "{}",
+                "Email is too long.".error()
+            )));
+        }
+
+        let mut username: [u8; COLUMN_USERNAME_SIZE] = [0; COLUMN_USERNAME_SIZE];
+        let mut email: [u8; COLUMN_EMAIL_SIZE] = [0; COLUMN_EMAIL_SIZE];
+
+        username[..input[2].len()].copy_from_slice(input[2].as_bytes());
+        email[..input[3].len()].copy_from_slice(input[3].as_bytes());
+
         let row = Row {
             id: input[1].parse().context(format!(
                 "{}",
-                "The row id is of type 'u64' for a User".error()
+                "The row id is of type 'u32' for a User".error()
             ))?,
-            username: input[2].to_owned(),
-            email: input[3].to_owned(),
+            username,
+            email,
         };
+
+        let serialized_row = row.serialize([0; ROW_SIZE]);
+        println!("serialized_row: {:?}", serialized_row);
 
         println!(
             "{}",
-            format!("Inserting {:?} in the database...", row).paint(Color::Cyan)
+            format!("Inserting {} in the database...", row).paint(Color::Cyan)
         );
 
         return Ok(StatementCommandResult::Success(Statement::Insert(row)));
